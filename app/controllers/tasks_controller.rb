20a1b277ad488f4921847cbf931f555617991263
Ahmed Soliman <ahmed.h.soliman@outlook.com>
2019-01-10 20:41:03 -0500

    added toggle status & trash functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,9 +9,16 @@ class TasksController < ApplicationController
     end
     def edit
         @task = Task.find(params[:id])
-        @task.update(:status => true)
-        puts "Edited:"
-        puts @task.inspect
+        if @task.status == true
+            @task.update(:status => false)
+        else @task.status == false
+            @task.update(:status => true)
+        end
+        redirect_to root_path
+    end
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
         redirect_to root_path
     end
 end
